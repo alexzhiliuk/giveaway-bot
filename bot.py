f964fd9550dac3223318de08348c8bbf9fa5b6a8
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-10-07 18:37:54 +0300

    fix

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ from handlers.admin import *
 from utils.finish_giveaway import *
 
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 if __name__ == "__main__":
 
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     if "WEBHOOKS" in list(os.environ.keys()):
         PROJECT_NAME = "zhiliuk.pythonanywhere.com"
 
-        @server.route('/' + TOKEN, methods=['POST'])
+        @app.route('/' + TOKEN, methods=['POST'])
         def getMessage():
             json_string = request.get_data().decode('utf-8')
             update = telebot.types.Update.de_json(json_string)
@@ -90,14 +90,14 @@ if __name__ == "__main__":
             return "!", 200
 
 
-        @server.route("/")
+        @app.route("/")
         def webhook():
             bot.remove_webhook()
             bot.set_webhook(url=f'https://{PROJECT_NAME}/' + TOKEN)
             return "!", 200
 
 
-        # server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+        app.run(host="0.0.0.0", port=443, ssl_context=('cert.pem', 'key.pem'))
 
     else:
         bot.polling(none_stop=True)

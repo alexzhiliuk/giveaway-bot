ed9ea7680a784671fade2c8f2beac498b24c4ebb
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-10-09 09:15:32 +0300

    fix: use log file path var in creating logger

--- logger.py
+++ logger.py
@@ -1,11 +1,11 @@
 import logging
-
+from config import LOGGER_NAME
 
 def get_logger():
     logger = logging.getLogger("bot")
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter("%(asctime)s - %(name)s::%(levelname)s - %(message)s")
-    handler = logging.FileHandler("bot_logs.log")
+    handler = logging.FileHandler(LOGGER_NAME)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
